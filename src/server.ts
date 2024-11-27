b01b1e38597fc291472b7f6c4baafda664303662
LeonLicher <98418299+LeonLicher@users.noreply.github.com>
2024-11-27 17:27:58 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://leonlicher.github.io/Kickbase/'],
+  origin: ['http://localhost:5173', 'https://leonlicher.github.io'],
   credentials: true
 }));
 app.use(express.json());
